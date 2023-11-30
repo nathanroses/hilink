e643f2a7f651aacfd10c50734a74a72410a54153
Nathan Rose <natetron99@gmail.com>
2023-11-30 00:04:04 -0500

    first

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // output: 'export'  // Commented out to resolve the image optimization issue
+  // You can add other configurations here if needed
+}
+
+module.exports = nextConfig;
