6c1b8a756a4824125de21ea51d7ea5656e90ad80
Nathan Rose <contacttstyle@gmail.com>
2025-03-16 01:46:54 -0500

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -105,3 +107,4 @@ const FooterColumn = ({ title, children }: FooterColumnProps) => {
 }
 
 export default Footer
+
