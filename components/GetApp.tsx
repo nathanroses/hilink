dc854e12e4510665ee2924c5d4bdf3569fc884b9
Nathan Rose <contacttstyle@gmail.com>
2025-03-16 01:36:14 -0500

    Update GetApp.tsx

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 const GetApp = () => {
   return (
-    <section className="flexCenter w-full flex-col pb-[100px]">
+    <section id="get_app" className="flexCenter w-full flex-col pb-[100px]">
       <div className="get-app">
         <div className="z-20 flex w-full flex-1 flex-col items-start justify-center gap-12">
           <h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">Get for free now!</h2>
@@ -16,6 +16,7 @@ const GetApp = () => {
               icon="/apple.svg"
               variant="btn_white"
               full
+              onClick={() => window.open('https://apps.apple.com', '_blank')}
             />
             <Button 
               type="button"
@@ -23,6 +24,7 @@ const GetApp = () => {
               icon="/android.svg"
               variant="btn_dark_green_outline"
               full
+              onClick={() => window.open('https://play.google.com', '_blank')}
             />
           </div>
         </div>
